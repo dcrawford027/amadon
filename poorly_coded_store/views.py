319e3fb3d1645a9edf50c0af9a89ac3198697fb6
Dustin Crawford <dustin.crawford.2@gmail.com>
2020-07-20 19:19:14 -0400

    fixed user accessable prices and repurchase on refresh

--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -8,9 +8,24 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    product = Product.objects.get(id=request.POST['product_id'])
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    total_charge = quantity_from_form * product.price
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    return redirect('/success')
+
+def success(request):
+    thisOrder = Order.objects.last()
+    orders = Order.objects.all()
+    totalItems = 0
+    totalMoney = 0
+    for order in orders:
+        totalItems += order.quantity_ordered
+        totalMoney += order.total_price
+    context = {
+        'totalCharge': thisOrder.total_price,
+        'totalItems': totalItems,
+        'totalMoney': totalMoney,
+    }
+    return render(request, "store/checkout.html", context)
